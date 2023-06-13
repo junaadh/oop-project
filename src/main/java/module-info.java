294c73e1d3f822dc59407508d91a7a9ae003ccab
Junaadh <junaadh.02@gmail.com>
2023-06-13 17:33:33 +0800

    restructured aboutus

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module oopsdg {
     requires transitive javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires javafx.web;
 
     opens oopsdg to javafx.fxml;
     exports oopsdg;
--- src/main/java/oopsdg/SdgController.java
+++ src/main/java/oopsdg/SdgController.java
@@ -12,34 +12,36 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-public class CreditsController {
+
+public class SdgController implements Initializable {
 
     private Parent root;
     private Scene scene;
     private Stage stage;
 
-    @FXML
+    @FXML 
     private Button backButton;
 
     @FXML
-    private Button sdgButton;
+    private Button aboutButton;
 
     @FXML
-    private Button teamButton;
+    private WebView webview;
 
-    @FXML
-    private Button programButton;
-
-    public void switchToSdg(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("aboutsdg.fxml"));
-        stage = (Stage)((Node) e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        webview.getEngine().load("https://www.globalgoals.org/goals/13-climate-action/");
     }
 
+    public void goBack(ActionEvent e) throws IOException {
+        if (webview.getEngine().getHistory().getCurrentIndex() > 0) {
+                webview.getEngine().getHistory().go(-1);
+            }
+    }
+    
     public void switchToWelcome(ActionEvent e) throws IOException {
         root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
         stage = (Stage)((Node) e.getSource()).getScene().getWindow();
