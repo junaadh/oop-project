ae644001068e41d332013a45df3ab484b228d8a3
Junaadh <junaadh.02@gmail.com>
2023-06-15 23:33:56 +0800

    tweaking

--- src/main/java/oopsdg/Main.java
+++ src/main/java/oopsdg/Main.java
@@ -6,10 +6,10 @@ public class Main {
 
 
         // To launch the GUI VERSION of the app, uncomment below line GUIMonitor.Launch()
-        // GUIMonitor.Launch();
+        GUIMonitor.Launch();
         
         // To launch the CONSOLE VERSION of the app, uncomment below line ConsoleMonitor.Launch()
-        ConsoleMonitor.Launch();
+        // ConsoleMonitor.Launch();
     
     }
 }
